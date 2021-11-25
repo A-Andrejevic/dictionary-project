d24c3119bf7abdc70f8651a6f11254ae97fe3e53
A-Andrejevic <92856864+A-Andrejevic@users.noreply.github.com>
2021-11-25 11:44:20 +0100

    Added footer with repo link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     
+      <footer>
+        This project was created by Aleksandra Andrejevic and it is completely{" "}
+        <a href="https://github.com/A-Andrejevic/dictionary-project">
+          open-sourced on GitHub.
+        </a>
+      </footer>
     </div>
   );
 }
